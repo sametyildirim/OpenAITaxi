539a6b9bf870a5452d8011cdaa24583409e8af48
sametyildirim <samet52@gmail.com>
2020-03-02 23:26:21 +0300

    random run

---
+++ taxirandom.py
@@ -0,0 +1,37 @@
+import gym
+from IPython.display import clear_output
+from time import sleep
+env = gym.make("Taxi-v3").env
+
+env.s = 328  # set environment to illustration's state
+
+epochs = 0
+penalties, reward = 0, 0
+
+frames = [] # for animation
+
+done = False
+
+while not done:
+    action = env.action_space.sample()
+    state, reward, done, info = env.step(action)
+
+    if reward == -10:
+        penalties += 1
+    
+    # Put each rendered frame into dict for animation
+    frames.append({
+        'frame': env.render(mode='ansi'),
+        'state': state,
+        'action': action,
+        'reward': reward
+        }
+    )
+    env.render()
+    sleep(.1)
+    epochs += 1
+    
+    
+print("Timesteps taken: {}".format(epochs))
+print("Penalties incurred: {}".format(penalties))
+
