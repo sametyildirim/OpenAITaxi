992612fad27a16e36ffd396cd393eba8f034c2fd
sametyildirim <samet52@gmail.com>
2020-03-02 22:57:05 +0300

    secong

--- taxi.py
+++ taxi.py
@@ -2,4 +2,11 @@ import gym
 
 env = gym.make("Taxi-v3").env
 
+state = env.encode(3, 1, 3, 0) # (taxi row, taxi column, passenger index, destination index)
+print("State:", state)
+
+env.s = state
 env.render()
+print("Action Space {}".format(env.action_space))
+print("State Space {}".format(env.observation_space))
+print("State:", state)
