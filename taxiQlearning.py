c6b6037070ac1b744e94e050b0a5aa0bff401e16
sametyildirim <samet52@gmail.com>
2020-03-06 21:36:42 +0300

    qlearning implemented

---
+++ taxiQlearning.py
@@ -0,0 +1,74 @@
+import gym
+from IPython.display import clear_output
+from time import sleep
+import random
+import numpy as np
+
+env = gym.make("Taxi-v3").env
+q_table = np.zeros([env.observation_space.n, env.action_space.n])
+# Hyperparameters
+alpha = 0.1
+gamma = 0.6
+epsilon = 0.1
+
+# For plotting metrics
+all_epochs = []
+all_penalties = []
+
+for i in range(1, 100001):
+    state = env.reset()
+
+    epochs, penalties, reward, = 0, 0, 0
+    done = False
+    
+    while not done:
+        if random.uniform(0, 1) < epsilon:
+            action = env.action_space.sample() # Explore action space
+        else:
+            action = np.argmax(q_table[state]) # Exploit learned values
+
+        next_state, reward, done, info = env.step(action) 
+        
+        old_value = q_table[state, action]
+        next_max = np.max(q_table[next_state])
+        
+        new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
+        q_table[state, action] = new_value
+
+        if reward == -10:
+            penalties += 1
+
+        state = next_state
+        epochs += 1
+        
+    if i % 100 == 0:
+        clear_output(wait=True)
+        print("Episode:"+str(i))
+
+print("Training finished.\n")
+
+total_epochs, total_penalties = 0, 0
+episodes = 100
+
+for _ in range(episodes):
+    state = env.reset()
+    epochs, penalties, reward = 0, 0, 0
+    
+    done = False
+    
+    while not done:
+        action = np.argmax(q_table[state])
+        state, reward, done, info = env.step(action)
+
+        if reward == -10:
+            penalties += 1
+
+        epochs += 1
+
+    total_penalties += penalties
+    total_epochs += epochs
+
+print("Results after {} episodes:".format(episodes))
+print("Average timesteps per episode: {}".format(total_epochs / episodes))
+print("Average penalties per episode: {}".format(total_penalties / episodes))
+
